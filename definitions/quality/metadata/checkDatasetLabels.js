2040523702838b99e5124d730d7881102a5995cd
Fernando Sedrez <fernando.sedrez@g.globo>
2024-09-16 21:40:27 -0300

    feat: add check dataset labels

---
+++ definitions/quality/metadata/checkDatasetLabels.js
@@ -0,0 +1,23 @@
+operate("check_dataset_labels", {
+  type: 'operations',
+  name: 'check_dataset_events',
+  schema: 'metadata_quality',
+  database: dataform.projectConfig.defaultDatabase,
+  tags: ['check-up'],
+  hasOutput: true,
+  hermetic: true,
+  }).queries(ctx =>
+  `insert into metadata_quality.check_events
+    SELECT
+  catalog_name,
+  schema_name,
+  NULL AS table_name,
+  'label' AS metadata_type,
+  CURRENT_TIMESTAMP()
+FROM
+  \`region-us.INFORMATION_SCHEMA.SCHEMATA\`
+WHERE
+  TRUE
+  AND NOT CONTAINS_SUBSTR(ddl, 'labels');
+`
+)
